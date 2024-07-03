d2c2efb36e9976b73a58a7317a453ab660f1af7e
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-07-03 21:35:22 +0200

    navbar not finish

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <nav className="flex bg-slate-100 content-center justify-between">
+    <nav className="flex bg-blue-100 content-center justify-between border-b-2 border-black">
       <span className=" m-4 text-purple-950 text-4xl">Talentia</span>
       <div>
         <ul className=" flex flex-row gap-10 mr-12">
-          <li className="m-6">Accueil</li>
-          <li className="m-6">Formation</li>
-          <li className="m-6">Jobs</li>
+          <li className="m-6">
+            <Link to="/">Accueil</Link>
+          </li>
+          <li className="m-6">
+            <Link to="/formation">Formation</Link>
+          </li>
+          <li className="m-6">
+            <Link to="/jobs">Jobs</Link>
+          </li>
           <li className="m-4">
-            <button className="border-solid border-2 p-2 text-white bg-indigo-600 rounded-lg border-black shadow-md">
-              Connexion
-            </button>
+            <Link>
+              <button className="border-solid border-2 px-8 py-2 text-white bg-indigo-600 rounded-lg border-black ">
+                Connexion
+              </button>
+            </Link>
           </li>
         </ul>{" "}
       </div>
