bbd8d964bbd8563a93e565d6a62465497cd9dd9c
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-07-04 17:21:20 +0200

    merge finished

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,13 +1,7 @@
 import Home from "./Home";
 import About from "./About";
 import Jobs from "./Jobs";
-<<<<<<< HEAD
-import Register from "./Register";
-
-export { Home, About, Jobs, Register };
-=======
 import Formations from "./Formations";
 import JobOffer from "./JobOffer";
 
 export { Home, About, Jobs, Formations, JobOffer };
->>>>>>> ef8fc17c21b2ccbedd88534e306e2b6a81e79390
