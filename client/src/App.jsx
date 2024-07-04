ef8fc17c21b2ccbedd88534e306e2b6a81e79390
Chaymae El Hayani <layani.chay@gmail.com>
2024-07-04 17:11:14 +0200

    website after resolving conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Home, About, Jobs, Formations, LogIn, Register, Connect, JobOffer } from "./pages/index";
+import { Home, About, Jobs, Formations, JobOffer } from "./pages/index";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -12,9 +12,6 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/formation" element={<Formations />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/connect" element={<Connect />} />
           <Route path="/joboffer" element={<JobOffer />} />
         </Routes>
       </Router>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,9 +2,6 @@ import Home from "./Home";
 import About from "./About";
 import Jobs from "./Jobs";
 import Formations from "./Formations";
-import LogIn from "./LogIn";
-import Connect from "./Connect";
-import Register from "./Register";
 import JobOffer from "./JobOffer";
 
-export { Home, About, Jobs, Formations, LogIn, Register, Connect, JobOffer };
+export { Home, About, Jobs, Formations, JobOffer };
