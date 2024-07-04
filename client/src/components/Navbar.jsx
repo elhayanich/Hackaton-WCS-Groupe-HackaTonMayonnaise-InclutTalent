da6fb33ec9cd15458dd02912b11c5733e9e21e2c
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-07-04 09:35:16 +0200

    popup connexion

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import LogIn from "../pages/LogIn";
 
 const Navbar = () => {
+  // const [modal, setModal] = useState(false);
+
+  // const toggleModal = () => {
+  //   setModal(!modal);
+  // };
+
   return (
     <nav className="flex bg-blue-100 content-center justify-between border-b-2 border-black">
       <span className=" m-4 text-purple-950 text-4xl">Talentia</span>
@@ -16,10 +23,15 @@ const Navbar = () => {
             <Link to="/jobs">Jobs</Link>
           </li>
           <li className="m-4">
-            <Link to="/LogIn">
+            <Link to="/login">
               <button className="border-solid border-2 px-8 py-2 text-white bg-indigo-600 rounded-lg border-black ">
                 Connexion
               </button>
+              {/* {modal && (
+                <div onClick={toggleModal}>
+                  <LogIn />
+                </div>
+              )} */}
             </Link>
           </li>
         </ul>{" "}
